791a24a11b6da655f8192250b9e027c3c84f75d9
scurty <scurtycel@gmail.com>
2016-01-05 02:10:16 -0200

    Initial commit

---
+++ app/src/main/java/com/claup/classe/Contador.java
@@ -0,0 +1,18 @@
+package com.claup.classe;
+
+/**
+ * Created by Sidney on 05/01/2016.
+ */
+public class Contador {
+
+    private int numero = 0;
+
+    public int getNumero() {
+
+        return numero;
+    }
+
+    public void setNumero() {
+        numero = numero + 1;
+    }
+}
